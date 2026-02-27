3031ee8c884df3a32353f77d7d9f6ff95447b8b1
Ruslan Zdor <ruslanzdor@microsoft.com>
2026-02-27 14:07:19 -0800

    update index

--- video-processing/src/index.ts
+++ video-processing/src/index.ts
@@ -1,12 +1,21 @@
-import { app } from '@azure/functions';
+import { app, InvocationContext  } from '@azure/functions';
 import { trimVideo } from "./trimVideo";
-
-app.setup({
-    enableHttpStream: true,
-});
 
 app.storageBlob('trimVideo', {
     path: 'volleyball-videos/inputs/{name}',
     connection: 'AzureWebJobsStorage',
     handler: trimVideo,
+});
+
+
+app.http("getUsers", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (_request, context: InvocationContext) => {
+    context.log("HTTP getUsers called");
+    return {
+      status: 200,
+      jsonBody: {}
+    };
+  }
 });
