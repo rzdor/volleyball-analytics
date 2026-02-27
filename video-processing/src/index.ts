e809c1445742afb379e346ca760c1dd314aa9709
Ruslan Zdor <ruslanzdor@microsoft.com>
2026-02-27 13:48:06 -0800

    update index

--- video-processing/src/index.ts
+++ video-processing/src/index.ts
@@ -1,5 +1,12 @@
 import { app } from '@azure/functions';
+import { trimVideo } from "./trimVideo";
 
 app.setup({
     enableHttpStream: true,
+});
+
+app.storageBlob('trimVideo', {
+    path: 'volleyball-videos/inputs/{name}',
+    connection: 'AzureWebJobsStorage',
+    handler: trimVideo,
 });
